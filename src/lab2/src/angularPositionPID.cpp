665f4bd3e9cafd7c665bb7b644952b7d30d52dd8
Kaitlyn Brad <kaitlynannbrady@gmail.com>
2015-10-07 11:23:31 -0500

    Successfully added an I parameter

--- src/lab2/src/angularPositionPID.cpp
+++ src/lab2/src/angularPositionPID.cpp
@@ -3,6 +3,7 @@
 #include <fanboat_ll/fanboatLL.h>
 #include <fanboat_ll/fanboatMotors.h>
 #include <math.h>
+#include <algorithm>
 
 fanboat_ll::fanboatMotors pubMsg;
 
@@ -14,6 +15,10 @@ const float DEFAULT_POWER = 0.12;
 
 double pParam;
 double dParam;
+double iParam;
+
+double integral;
+const double iMax = 0.9;
 
 float map(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -38,7 +43,7 @@ void angleInputCallback(const lab2::angle::ConstPtr& msg) {
     float targetAngle = msg->angle;
     // Find error from target (potentially switch??)
     float degreesToTurn = currentDir - targetAngle;
-    ROS_INFO("I need to turn by %f", degreesToTurn);
+
     float prevDegreesToTurn = previousDir - targetAngle;
 
     // Parse degrees to less than 180 degree turns
@@ -50,21 +55,30 @@ void angleInputCallback(const lab2::angle::ConstPtr& msg) {
       prevDegreesToTurn = (prevDegreesToTurn + 360.0);// * -1.0;
     }
 
+    ROS_INFO("I need to turn by %f", degreesToTurn);
 
     float mot_r = 0.0;
     float mot_l = 0.0;
 
+    integral = integral + degreesToTurn;
+    // If the fanboat turns past it's target, reset integral
+    if(integral * degreesToTurn < 0) integral = degreesToTurn;
+
+    ROS_INFO("Integral: %f", integral);    
+
     if(degreesToTurn > 0.0) {
       //turn left (ccw)
       mot_r = .12;
-      mot_l = pParam * degreesToTurn + dParam * (degreesToTurn - prevDegreesToTurn) / (currentTS - prevTS);
+      mot_l = pParam * degreesToTurn + std::min(fabs(iParam * integral), iMax) + dParam * (degreesToTurn - prevDegreesToTurn) / (currentTS - prevTS);
     } else {
       //turn right (cw)
       mot_l = .12;
       degreesToTurn = -1 * degreesToTurn;
       prevDegreesToTurn = -1 * prevDegreesToTurn;
-      mot_r = pParam * degreesToTurn +  dParam * (degreesToTurn - prevDegreesToTurn) / (currentTS - prevTS);
+      mot_r = pParam * degreesToTurn + std::min(fabs(iParam * integral), iMax) + dParam * (degreesToTurn - prevDegreesToTurn) / (currentTS - prevTS);
     }
+
+    ROS_INFO("Left Motor Unconstrained: %f", mot_l);
 
     if(mot_r > 0.9) mot_r = 0.9;
     if(mot_l > 0.9) mot_l = 0.9;
@@ -91,6 +105,9 @@ int main(int argc, char **argv) {
 
   n.getParam("pParam", pParam);
   n.getParam("dParam", dParam);
+  n.getParam("iParam", iParam);
+
+  integral = 0;
   
   pubMsg.right = .12;
   pubMsg.left = .12;
