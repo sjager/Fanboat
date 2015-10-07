2a7c232a7ad37ce84dee2986cff78057a71ad913
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-10-07 10:04:00 -0500

    added mux_control source code

---
+++ src/lab2/src/mux_control.cpp
@@ -0,0 +1,62 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+
+#define A_INPUT 1
+#define B_INPUT 2
+#define X_INPUT 3
+#define Y_INPUT 4
+
+#define INVALID_INPUT 5
+
+// /mux_control
+
+lab2::mux_control muxControlPubMsg;
+
+void inputCallback(const sensor_msgs::Joy::ConstPtr& msg) {
+  ROS_INFO("Got input");
+  
+  int input = INVALID_INPUT;
+  
+  int aDown = msg->buttons[0];
+  int bDown = msg->buttons[1];
+  int xDown = msg->buttons[2];
+  int yDown = msg->buttons[3];
+  
+  if(aDown == 1) {
+    input = A_INPUT;
+  } else if(bDown == 1) {
+    input = B_INPUT;
+  } else if(xDown == 1) {
+    input = X_INPUT;
+  } else if(yDown == 1) {
+    input = Y_INPUT;
+  }
+  
+  muxControlPubMsg.msg = input;
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "mux_control_node");
+  
+  ros::NodeHandle n;
+  
+  ros::Publisher muxControlPub = n.advertise<lab2::mux_control>("/mux_control", 1000);
+  
+  ros::Subscriber sub = n.subscribe("/joy", 1000, inputCallback);
+  
+  ros::Rate loop_rate(13);
+  
+  while(ros::ok()) {
+  
+    bool validInput = (muxControlPubMsg.msg == A_INPUT) || (muxControlPubMsg.msg == B_INPUT) || (muxControlPubMsg.msg == X_INPUT);
+  
+    if(validInput)
+    {
+      muxControlPub.publish();
+    }
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  
+  return 0; 
+}
