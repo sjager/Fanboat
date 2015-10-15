ba7e36fec92a3a1a9cca6b02900654e2e414e867
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-10-15 18:45:22 -0500

    finished furthest mapping and recalibrate PID

--- src/lab2/src/mapping.cpp
+++ src/lab2/src/mapping.cpp
@@ -7,57 +7,104 @@
 #include <sensor_msgs/Joy.h>
 
 #define BUFFER_SIZE 10
+#define ANGLE_TOL_START -5.0
+#define ANGLE_TOL_END -15.0 
+#define TURN_SPEED 15
 
 lab2::range rangeMsg;
 lab2::angle angleMsg;
 lab2::magnitude magMsg;
 fanboat_ll::fanboatLL IMUMsg;
-double target_dist;
 
-float buffer[BUFFER_SIZE];
+// to filter out noise, we are using running averages of n=BUFFER_SIZE values.
+float minBuffer[BUFFER_SIZE];
+float maxBuffer[BUFFER_SIZE];
 
-float max_dist = 1.5;
 int mapping_type = 0;
+
 float rangeA;
 float rangeB;
+
+// determines if the robot should be spinning or seeking the best angle.
+// the robot will continue to spin while flag =0. When it has spun close to 360 degrees,
+// flag will be updated to 1.
 int flag = 0;
 
+//current angle and distance
 float ang;
 float dist = 999.0;
-float exDist = 9999.0;
 
-float exAng = 0;
+float minDist = 999.0;
+float maxDist = -1.0;
+float minAng = 0.0;
+float maxAng = 0.0;
+
+// the angle that should be used as a reference for detecting a full spin.
 float startAngle;
 
-float averageDist = 999.0;
+float averageMinDist = 999.0;
+float averageMaxDist = -1.0;
 
-void insertIntoBuffer(float newVal) {
+// updates the buffer of min values and updates the average.
+void insertIntoMinBuffer(float newVal) {
+  
   for(int i = 0; i < BUFFER_SIZE - 1; i++) {
-    buffer[i] = buffer[i+1];
+    minBuffer[i] = minBuffer[i+1];
   }
-  buffer[BUFFER_SIZE-1] = newVal;
+  minBuffer[BUFFER_SIZE-1] = newVal;
   
   float runningSum = 0;
   
   for(int j = 0; j < BUFFER_SIZE; j++) {
-    runningSum += buffer[j];
+    runningSum += minBuffer[j];
   }
-  
-  averageDist = runningSum / BUFFER_SIZE;
+  averageMinDist = runningSum / BUFFER_SIZE;
   
 }
+
+// updates the buffer of max values and updates the average.
+void insertIntoMaxBuffer(float newVal) {
+
+  for(int i = 0; i < BUFFER_SIZE - 1; i++) {
+    maxBuffer[i] = maxBuffer[i+1];
+  }
+  maxBuffer[BUFFER_SIZE-1] = newVal;
+  
+  float runningSum = 0;
+  
+  for(int j = 0; j < BUFFER_SIZE; j++) {
+    runningSum += maxBuffer[j];
+  }
+  averageMaxDist = runningSum / BUFFER_SIZE;
+  
+}
+
 
 void controllerCallback(const sensor_msgs::Joy::ConstPtr& msg) {
   
   int yDown = msg->buttons[3];
   
+  // when the Y button is pressed, it restarts the node;
   if(yDown == 1) {
     startAngle = IMUMsg.yaw;  
     ROS_INFO("\n\n\nRESET\n\n\n");
   }
+  
+  // reset the flag so the robot restarts the circle
   flag = 0;
-  exDist = 9999.0;
-  exAng = 0.0;
+  
+  // reset best values
+  minDist = 9999.0;
+  minAng = 0.0;
+  maxDist = -1.0;
+  maxAng = 0.0;
+  
+  // reset buffer values
+  for(int i = 0; i < BUFFER_SIZE; i++) {
+    minBuffer[i] = 9999.9;
+    maxBuffer[i] = -1.0;
+  }
+  
 }
 
 void IMUinputCallback(const fanboat_ll::fanboatLL::ConstPtr& msg) {
@@ -68,18 +115,22 @@ void IMUinputCallback(const fanboat_ll::fanboatLL::ConstPtr& msg) {
 void inputCallback(const lab2::range::ConstPtr& msg)
 {
   rangeMsg = *msg;
+	
 	//set globals for the current ranges
 	rangeA = rangeMsg.a;
 	rangeB = rangeMsg.b;
-	dist = std::min(rangeA, rangeB);
 	
-	if(dist == 0.0)
+	//update minimum distances
+	dist = std::min(rangeA, rangeB);
+	if(dist == 0.0) //when the node first starts, the IR sensors report 0.0 until they are fully initialized. We throw these values out.
 	{
 	  dist = 500;
 	}
+	insertIntoMinBuffer(dist);
 	
-	insertIntoBuffer(dist);
-	
+	//update maximum distances
+	dist = std::max(rangeA, rangeB);
+	insertIntoMaxBuffer(dist);
 }
 
 int main(int argc, char **argv)
@@ -90,54 +141,72 @@ int main(int argc, char **argv)
 
     ros::Publisher mag_pub = n.advertise<lab2::magnitude>("/mapping_magnitude", 1000);
     ros::Publisher ang_pub = n.advertise<lab2::angle>("/mapping_angle", 1000);
+    
     ros::Subscriber sub = n.subscribe("/ir_range", 1000, inputCallback);
 	  ros::Subscriber fanboat = n.subscribe("/fanboatLL", 1000, IMUinputCallback);
 	  ros::Subscriber joysub = n.subscribe("/joy", 1000, controllerCallback);
 	
     n.getParam("mapping_type", mapping_type);
     
-    
     ros::Rate loop_rate(8);
 
     while(ros::ok())
     {
-    
-	        if((IMUMsg.yaw - startAngle < -5) && (IMUMsg.yaw - startAngle > -15)) {
-	          flag = 1;
-	        }
+      float angleDif = IMUMsg.yaw - startAngle;
+      
+      // if the robot detects an angle difference that falls within a certain range, we can assume it has turned a full circle.    
+      if((angleDif < ANGLE_TOL_START) && (angleDif > ANGLE_TOL_END)) {
+        flag = 1;
+      }
 
-	        if(flag == 0){
-	          ROS_INFO("CUR: %f, ST: %f, DIF: %f, DIST: %f, BDIST: %F, BANG: %f", IMUMsg.yaw, startAngle, (IMUMsg.yaw - startAngle), averageDist, exDist, exAng);
-			      angleMsg.angle = IMUMsg.yaw + 30; //for statement had i, not 45
-		        //ROS_INFO("I'm trying to get to the angle: %f\n", angleMsg.angle);
-		        //get the current angle and distance
-		        ang = IMUMsg.yaw - 360.0;
-		        //record the new shortest/longest distance
-		        if(mapping_type == 0) {
-			        if((dist != 0.0)&&(averageDist < exDist)) {
-				        ROS_INFO("New nearest found: D: %f, ANG: %f", averageDist, ang);
-				        exDist = averageDist;
-				        exAng = ang;
-			        }
-		        } else {
-			        if(averageDist > exDist) {
-				        ROS_INFO("New furthest found: %f, %f", dist, ang);
-				        exDist = averageDist;
-				        exAng = ang;
-			        }
-		        }
-	        }
+      if(flag == 0) {
+        ROS_INFO("CUR: %f, ST: %f, DIF: %f, DIST: %f, BDIST: %F, BANG: %f",
+         IMUMsg.yaw, startAngle, (IMUMsg.yaw - startAngle), averageMinDist, minDist, minAng);
+	      
+	      // rotate the boat
+	      angleMsg.angle = IMUMsg.yaw + TURN_SPEED; 
+        
+        //get the current angle and distance
+        ang = IMUMsg.yaw - 360.0;
+        
+        //record the new shortest
+        if((dist != 0.0)&&(averageMinDist < minDist)) {
+	        ROS_INFO("New shortest found: D: %f, ANG: %f", averageMinDist, ang);
+	        minDist = averageMinDist;
+	        minAng = ang;
+        }
+        
+        //record the new furthest
+        if(averageMaxDist > maxDist) {
+	        ROS_INFO("New furthest found: D: %f, ANG: %f", averageMaxDist, ang);
+	        maxDist = averageMaxDist;
+	        maxAng = ang;
+        }
+      }
 
-	        if(flag == 1) {
-	          angleMsg.angle = exAng - 45.0;
-	          ROS_INFO("CUR: %f, SK: %f, DIF: %f, BDIST: %f", IMUMsg.yaw, exAng, (IMUMsg.yaw - exAng), exDist);
-	        }
+      float bestAngle = 0.0;
+      float bestDistance = -1.0;
 
-    
-        mag_pub.publish(magMsg);
-        ang_pub.publish(angleMsg);
-        ros::spinOnce();
-        loop_rate.sleep();
+      //if flag ==1, then the robot should stop spinning and begin to turn to the best angle.
+      if(flag == 1) {
+        // change the target angle depending on if we're looking for the closest or furthest object
+        if(mapping_type == 0) {
+          bestAngle = minAng;
+          bestDistance = minDist;
+        } else {
+          bestAngle = maxAng;
+          bestDistance = maxDist;
+        }
+       
+        angleMsg.angle = bestAngle - 45.0;
+        ROS_INFO("CUR: %f, SK: %f, DIF: %f, BDIST: %f", IMUMsg.yaw, bestAngle, (IMUMsg.yaw - bestAngle), bestDistance);         
+      }
+
+      mag_pub.publish(magMsg);
+      ang_pub.publish(angleMsg);
+      
+      ros::spinOnce();
+      loop_rate.sleep();
     }
     
     return 0;
