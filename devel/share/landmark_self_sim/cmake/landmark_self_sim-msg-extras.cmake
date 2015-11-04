d2c419d1101d9851b063643a9bce466d8eec2d50
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-11-04 09:54:49 -0600

    adding lab3 launch

--- devel/share/landmark_self_sim/cmake/landmark_self_sim-msg-extras.cmake
+++ devel/share/landmark_self_sim/cmake/landmark_self_sim-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(landmark_self_sim_MESSAGE_FILES "/home/kaitlyn/Desktop/Git_Repos/Fanboat/src/landmark_self_sim/msg/landmarkLocation.msg")
+set(landmark_self_sim_MESSAGE_FILES "/home/jimmy/Fanboat/src/landmark_self_sim/msg/landmarkLocation.msg")
 set(landmark_self_sim_SERVICE_FILES "")
--- devel/share/landmark_self_sim/cmake/landmark_self_sim-msg-paths.cmake
+++ devel/share/landmark_self_sim/cmake/landmark_self_sim-msg-paths.cmake
@@ -1,4 +1,4 @@
 # generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
 
-set(landmark_self_sim_MSG_INCLUDE_DIRS "/home/kaitlyn/Desktop/Git_Repos/Fanboat/src/landmark_self_sim/msg")
+set(landmark_self_sim_MSG_INCLUDE_DIRS "/home/jimmy/Fanboat/src/landmark_self_sim/msg")
 set(landmark_self_sim_MSG_DEPENDENCIES std_msgs)
