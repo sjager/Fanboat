c2d26d5cd2a3a623ea379752efa05d81797f8e31
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-11-04 10:40:40 -0600

    moar script

--- src/transferLab3.sh
+++ src/transferLab3.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-echo "copying lab3 folder..."
-scp -r "lab3" odroid@odroid2:Fanboat/src/
+robotics="robotics"
+
+networkName=$(nm-tool | grep \*)
+
+if echo "$networkName" | grep -q "$robotics"; then
+    echo "copying lab3 folder..."
+    scp -r "lab3" odroid@odroid2:Fanboat/src/
+    echo
+    echo "you done it!"
+    echo
+else
+    echo
+    echo "Wrong network, bubba. Connect to robotics pls";
+    echo
+fi
+
