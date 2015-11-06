9e84844afc4702dc9f9e597427b36701570ae9a1
Kaitlyn Brad <kaitlynannbrady@gmail.com>
2015-11-06 05:44:46 -0600

    add broken graphics mux code

--- src/lab3/src/graphics_arbitrator.cpp
+++ src/lab3/src/graphics_arbitrator.cpp
@@ -7,28 +7,30 @@
 landmark_self_sim::landmarkLocation inputLandmarkMsg;
 ball_detector::ballLocation inputBallMsg;
 
-ros::NodeHandle n;
+int detectBall = false;
+int detectLandmark = true;
+
 ros::Publisher ballpub;
 ros::Publisher landmarkpub;
 
 void ballCallback(const ball_detector::ballLocation::ConstPtr& msg) {
-  bool detectBall = false;
-  if(n.hasParam("detectBall") && n.param("detectBall", detectBall)) {
+  ROS_INFO("About to try publishing %i", detectBall);
+  if(detectBall > 0) {
+    ROS_INFO("Publishing Message");
     ballpub.publish(*msg);
   }
 }
 
 void landmarkCallback(const landmark_self_sim::landmarkLocation::ConstPtr& msg) {
-  bool detectLandmark = false;
-  if(n.hasParam("detectLandmark") && n.param("detectLandmark", detectLandmark)) {
+  if(detectLandmark > 0) {
     landmarkpub.publish(*msg);
   }
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "graphics_arbitrator_node");
-
-  ballpub = n.advertise<ball_detector::ballLocation>("/graphics_arbitrator/", 1000);
+  ros::NodeHandle n;
+  ballpub = n.advertise<ball_detector::ballLocation>("/graphics_arbitrator/ballLocation", 1000);
   landmarkpub = n.advertise<landmark_self_sim::landmarkLocation>("/graphics_arbitrator/landmarkLocation", 1000);
 
   ros::Subscriber ballsub = n.subscribe<ball_detector::ballLocation>("/ballLocation", 1000, ballCallback);
@@ -37,6 +39,14 @@ int main(int argc, char **argv) {
   ros::Rate loop_rate(8);
 
   while(ros::ok()) {
+    ROS_INFO("I'm working");
+    if(n.hasParam("detectBall")) 
+    {            
+        n.param("detectBall", detectBall);
+        ROS_INFO("Set detect ball");
+    }
+    if(n.hasParam("detectLandmark")) n.param("detectLandmark", detectLandmark);
+
     ros::spinOnce();
     loop_rate.sleep();
   }
