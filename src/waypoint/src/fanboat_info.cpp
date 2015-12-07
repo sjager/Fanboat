4fe83c67c53da998a76c633d14bcdf30784e92d3
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-12-07 12:59:40 -0600

    it makes now!

--- src/waypoint/src/fanboat_info.cpp
+++ src/waypoint/src/fanboat_info.cpp
@@ -6,9 +6,13 @@
 #include <fanboat_ll/fanboatLL.h>
 #include <fanboat_ll/fanboatMotors.h>
 #include <waypoint/fanboatInfo.h>
+#include <landmark_self_sim/landmarkLocation.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Int32.h>
 
-using std_msgs::float32;
-using std_msgs::int32;
+using std_msgs::Float32;
+using std_msgs::Int32;
+using waypoint::fanboatInfo;
 
 float tgtAngle;         // fanboatcontrol
 float tgtMagnitude;     // fanboatcontrol
@@ -32,9 +36,22 @@ float IRR;              // IR
 
 float camAngle;         // camServo
 
+fanboatInfo fbInfo;
+
+void controlCallback(const lab3::fanboatControl::ConstPtr& msg) {
+}
+
+void tgtLandmarkCallback(const std_msgs::Int32::ConstPtr& msg) {
+}
+
+void IMUCallback(const fanboat_ll::fanboatLL::ConstPtr& msg) {
+}
+
+void landmarkCallback(const landmark_self_sim::landmarkLocation::ConstPtr& msg) {
+}
+
 /*
-void searchCallback(const lab3::fanboatControl::ConstPtr& msg) {
-    searchMsg = *msg;
+void IMUCallback(const waypoint::camera?::ConstPtr& msg) {
 }
 */
 
@@ -44,38 +61,23 @@ int main(int argc, char **argv) {
 
     ros::NodeHandle n;
 
-    ros::Publisher infoPub = n.advertise<lab3::fanboatInfo>("/fanboatInfo", 1000);
+    ros::Publisher infoPub = n.advertise<waypoint::fanboatInfo>("/fanboatInfo", 1000);
     
     ros::Subscriber controlSub = n.subscribe("/waypoint/control", 1000, controlCallback);
     
-    ros::Subscriber tgtLandmarkSub = n.subscribe("/waypoint/landmark", 1000, controlCallback);
+    ros::Subscriber tgtLandmarkSub = n.subscribe("/waypoint/landmark", 1000, tgtLandmarkCallback);
     
-    ros::Subscriber IMUSub = n.subscribe("/fanboatLL", 1000, controlCallback);
+    ros::Subscriber IMUSub = n.subscribe("/fanboatLL", 1000, IMUCallback);
     
-    ros::Subscriber landmarkSub = n.subscribe("/waypoint/control", 1000, controlCallback);
+    ros::Subscriber landmarkSub = n.subscribe("/landmarkLocation", 1000, landmarkCallback);
+    
+    //ros::Subscriber cameraSub = n.subscribe("/waypoint/control", 1000, cameraCallback);
 
     ros::Rate loop_rate(8);
 
     while(ros::ok())
-    {
-        determineState();
-        
-        switch(currentState)
-        {
-            case SEARCH :
-                finalControlMsg = searchMsg; 
-                break;
-            case PURSUE :
-                finalControlMsg = pursueMsg;
-                break;
-            case AVOID  :
-                finalControlMsg = avoidMsg;
-                break;
-            default :
-                ROS_INFO("state undetermined");
-        }
-        
-        controlPub.publish(finalControlMsg);
+    {        
+        infoPub.publish(fbInfo);
         ros::spinOnce();
         loop_rate.sleep();
     }
